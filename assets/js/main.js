e31703627da32913947bbccdad0cf9b6e6b2e632
Rolando Hernández <rolandohernandezleal@gmail.com>
2016-11-16 19:29:21 -0300

    Update site

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+  // Sticky elements
+
+  // $(window).scroll(function() {
+  //   var scroll = getCurrentScroll();
+  //     if ( scroll >= 400 && $(window).width() > 992 ) {
+  //       $('.main-header').addClass('is-fixed');
+  //     } else {
+  //       $('.main-header').removeClass('is-fixed');
+  //     }
+  // });
+  //
+  // function getCurrentScroll() {
+  //   return window.pageYOffset || document.documentElement.scrollTop;
+  //   }
+
+ });
