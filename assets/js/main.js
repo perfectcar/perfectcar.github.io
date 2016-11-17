7dad879f713328c55ee137195bba2c8759b84128
Rolando Hernández <rolandohernandezleal@gmail.com>
2016-11-16 23:28:13 -0300

    Add header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,23 @@
 $(function(){
 
-  // Sticky elements
+  new WOW().init();
 
-  // $(window).scroll(function() {
-  //   var scroll = getCurrentScroll();
-  //     if ( scroll >= 400 && $(window).width() > 992 ) {
-  //       $('.main-header').addClass('is-fixed');
-  //     } else {
-  //       $('.main-header').removeClass('is-fixed');
-  //     }
-  // });
-  //
-  // function getCurrentScroll() {
-  //   return window.pageYOffset || document.documentElement.scrollTop;
-  //   }
+  // Smoth Scrolling
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+      var calc = target.offset().top;
+
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: calc
+        }, 1000);
+        return false;
+      }
+    }
+  });
 
  });
